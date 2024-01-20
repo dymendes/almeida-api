04b0c0e8b36b58e3efb32006112d03b058897259
Dimitry Mendes <dymealves@gmail.com>
2024-01-19 23:37:21 -0300

    Update Route "/"

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,5 +3,7 @@ import NewsletterController from "../controllers/NewsletterController.js"
 
 export const router = express.Router()
 
+router.get("/", (req, res) => res.json({ message: "Bem-vindo(a) á Almeida-API" }))
+
 router.get("/subscribe/:email", NewsletterController.subscribe)
 router.get("/find", NewsletterController.findEmails)
